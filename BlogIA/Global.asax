0a906ab41ba77019718da6ecc1c5408de952bc97
Euler Chavez Hernandez <EulerChavez@outlook.com>
2017-04-20 20:46:01 -0500

    Agregar archivos de proyecto.

---
+++ BlogIA/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BlogIA.MvcApplication" Language="C#" %>
---
+++ BlogIA/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BlogIA.Startup))]
+namespace BlogIA
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
